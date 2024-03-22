6b0cffa33f7fdcbb2d4fe5f63a00954b12c2a0de
Banggyu <163507549+Banggyu@users.noreply.github.com>
2024-03-22 15:57:48 +0900

    3주차 과제
    Dictionary . list를 이용하여 값 구하기

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-a = input("채팅을 입력하세요 :")
-print(a)
+money = {"달러": 1320, "엔": 950, "위안": 182}
+print(money)
 
+x = [13, 200, 13]
+a = print(money["달러"] * x[0])
+b = print(money["엔"] * x[1])
+c = print(money["위안"] * x[2])
 
-b=10
-c=2
-print(b*c)
+m = [17160, 190000, 2366]
+
+t = m[0]+m[1]+m[2]
+print(t)
+
+print("철수가 가지고 있는 돈의 원화 가치는 209526원 입니다")
